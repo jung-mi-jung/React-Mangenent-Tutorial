f120a98d21cbf8a1d4efbc1576cfe10ffc469f4b
jung-e <jung079e@gmail.com>
2025-04-07 14:58:16 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
-
-function App extends Component {
+class App extends Component {
 	render () {
 		return (
 			<div className="gray-background">
